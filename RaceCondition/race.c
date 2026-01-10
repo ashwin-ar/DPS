eb43304069747663cb1aed2562eb81517e3a9a17
Ashwin Balaji Arun Kumar <ashwin.arunkumar001@stud.fh-dortmund.de>
2026-01-10 22:39:54 +0100

    With Mutex:
    - included Mutex
    - gitIgnore updated

--- RaceCondition/race.c
+++ RaceCondition/race.c
@@ -4,9 +4,16 @@
  Loop 2 10 20 11 12 13 21 2 3 4 14 15 22 23 5 6 16 17 18 7 8 24 25 19 26 9 27 28 29
 */
 
+/* Output with mutex
+ Loop 0 0 1 2 3 4 5 6 7 8 9
+ Loop 1 10 11 12 13 14 15 16 17 18 19
+ Loop 2 20 21 22 23 24 25 26 27 28 29*/
+
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+pthread_mutex_t lock; //creating a mutex lock
 
 struct data{
     int id;
@@ -19,11 +26,15 @@ struct data{
 // typedef struct data{int id;int totalTime;} frame;
 
 void* counter(void* arg){
+    pthread_mutex_lock(&lock);
+
     struct data* currentData = (struct data*) arg; // Casting void* to int*
     printf("\n Loop %d", currentData->id);
     for(int i= (currentData->id)*10 ;i< (currentData -> totalTime); i++){
         printf(" %d", i);
     }
+    
+    pthread_mutex_unlock(&lock);
 }
 
 void main(){
@@ -35,6 +46,8 @@ void main(){
     void* (*fp_cntr)(void*);
     fp_cntr = counter;
 
+    pthread_mutex_init(&lock, NULL);
+
     pthread_create(thread+0, NULL, fp_cntr,(void*) (dataFrame+0));
     pthread_create(thread+1, NULL, fp_cntr,(void*) (dataFrame+1));
     pthread_create(thread+2, NULL, fp_cntr,(void*) (dataFrame+2));
@@ -43,4 +56,5 @@ void main(){
         pthread_join(thread[i], NULL);
     }
 
+    pthread_mutex_destroy(&lock);
 }
