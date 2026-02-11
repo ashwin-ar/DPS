2b640d583e24c7ff37bd3b537aa5a9de7721a0de
Ashwin Balaji AR <ashwin.balaji75@gmail.com>
2026-02-11 19:06:48 +0100

    Understanding 1) Race condition with 2) time slicing
    - Race condition - incorrect results because of threads changing variables at same time
    - Time slice - each thread gets a piece of core at some instant.

--- time_slicing/time_slicing.c
+++ time_slicing/time_slicing.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <unistd.h> // for sleep function
 
-int coutner =0;
+int counter =0;
 
 void *thread(void* arg){
     int* temp_arg = (int*) arg;
 
+    for(int i=0;i<10000;i++){
+        // Simulating Race condition - this was actually given in pdf as onyl two counter increments, but I was not able to simualte it over here
+        // so using a for loop & rest is same
+
+        // Time slicing - thread 1 @ instant 0 and thread 2 @ instant 1
+        int temp = counter;
+        usleep(1);
+        counter = temp+1;
+    }
     return NULL;
 }
 
@@ -15,6 +25,12 @@ int main(){
     int thread_id[2] = {0,1};
     
     pthread_create(&thread_list[0],NULL,thread,&thread_id[0]);
+    pthread_create(&thread_list[1],NULL,thread,&thread_id[1]);
+    
     pthread_join(thread_list[0], NULL);
-    return NULL;
+    pthread_join(thread_list[1], NULL);
+
+    printf("Final counter = %d\n", counter);
+    
+    return 0;
 }
