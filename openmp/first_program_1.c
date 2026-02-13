7b7276f62bfee92c0d5a3536e746bab2ea44d0fa
Ashwin Balaji AR <ashwin.balaji75@gmail.com>
2026-02-13 18:17:33 +0100

    5 - Understanding fork & join

--- openmp/first_program_1.c
+++ openmp/first_program_1.c
@@ -12,15 +12,16 @@ int main()
       ii. which thread is executing this
       iii. Creating 'x' no of threads.
         a. method 1 - using a function above pragma - omp_set_num_threads
-        b. method 2 - using a construct num_threads in pragma statement
+        b. method 2 - using a clause num_threads in pragma statement
         anything can be used. if both used then construct is used.
 
-        note: Only 0-to xcores will execute this; x canbe any number
+        note: Only 0-to x cores will execute this; x can be any number
         note: without using this by default it was 16 threads, when we just used pragma omp parallel
       iv. private & shared variable. 
     */
     omp_set_num_threads(30);         /// iii
-#pragma omp parallel num_threads(5) // iii
+    printf("\n Begin fork - before the fork block ");
+    #pragma omp parallel num_threads(5) // iii
     {                                // i
         int sum2 = 0;                // iv
         i = omp_get_thread_num();    // ii
@@ -28,5 +29,6 @@ int main()
         sum2 += i;
         printf("\n hello world %d, sum1=%d, sum2=%d", i, sum1, sum2);
     }
+    printf("\n all done - executes only when everything is joined");
     return 0;
 }
