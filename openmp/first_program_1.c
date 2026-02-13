b11e1ec556ae49d17c04ed59ef4252f93f6e8541
Ashwin Balaji AR <ashwin.balaji75@gmail.com>
2026-02-13 16:51:40 +0100

    2-i- we can limit no of threads
    num_threads To create any no of threads, we can use this.

--- openmp/first_program_1.c
+++ openmp/first_program_1.c
@@ -3,7 +3,14 @@
 #include <omp.h> //openmp
 
 int main(){
-    #pragma omp parallel 
+    /*  iii. Creating 'x' no of threads
+        a. method 1 - using a function above pragma - omp_set_num_threads
+        b. method 2 - using a construct num_threads in pragma statement
+        
+        note: without using this by default it was 16 threads, when we just used pragma omp parallel 
+    */
+    omp_set_num_threads(30); // iii. Only 0-to xcores will execute this; x canbe any number
+    #pragma omp parallel num_threads(20) // iii. either this or omp_set_num_threads can be used
     { // i. how to start & end my parallel block
     int i=0;
     
