2c66b17a78662b7a27b0a48a6f7bbe0079b01c61
Ashwin Balaji AR <ashwin.balaji75@gmail.com>
2026-02-13 16:59:37 +0100

    reconstructing this

--- openmp/first_program_1.c
+++ openmp/first_program_1.c
@@ -3,18 +3,23 @@
 #include <omp.h> //openmp
 
 int main(){
-    /*  iii. Creating 'x' no of threads
+    /* Comments:
+      i. how to start & end my parallel block  
+      ii. which thread is executing this
+      iii. Creating 'x' no of threads.
         a. method 1 - using a function above pragma - omp_set_num_threads
         b. method 2 - using a construct num_threads in pragma statement
+        anything can be used. if both used then construct is used.
         
+        note: Only 0-to xcores will execute this; x canbe any number
         note: without using this by default it was 16 threads, when we just used pragma omp parallel 
     */
-    omp_set_num_threads(30); // iii. Only 0-to xcores will execute this; x canbe any number
-    #pragma omp parallel num_threads(20) // iii. either this or omp_set_num_threads can be used
-    { // i. how to start & end my parallel block
+    omp_set_num_threads(30); /// iii
+    #pragma omp parallel num_threads(20) // iii
+    {// i
     int i=0;
     
-    i = omp_get_thread_num(); //ii. which thread is executing this
+    i = omp_get_thread_num(); //ii
     printf("\n hello %d",i);
     printf(" world %d",i);
     }
